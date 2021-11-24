05d9ecee21a29af28b87bbc8ade27cb2a0ad739e
Judjin-debug <84127507+Judjin-debug@users.noreply.github.com>
2021-11-24 12:35:41 +0300

    removed string convertation from ai.py and replaced is with array of 2 radnom integers

--- ai.py
+++ ai.py
@@ -6,19 +6,16 @@ from dot import Dot
 class AI(Player):
 
     def ask(self):
-        return str(random.randrange(1, self.enemy_board.get_n + 1)) + " " + str(random.randrange(1, self.enemy_board.get_m + 1))
+        return [random.randrange(1, self.enemy_board.get_n + 1), random.randrange(1, self.enemy_board.get_m + 1)]
 
     def move(self):
         flag = True
         while flag:
             try:
-                answer = self.ask()
-                if not self.input_check(answer):
-                    raise ValueError("Illegal input")
-                processed_answer = self.input_processing(answer)
-                self.enemy_board.shot(Dot(int(processed_answer[0]) - 1, int(processed_answer[1]) - 1))
+                processed_answer = self.ask()
+                self.enemy_board.shot(Dot(processed_answer[0] - 1, processed_answer[1] - 1))
             except ValueError as e:
                 pass
             else:
-                print(f"Shot coordinate is {int(processed_answer[0])}, {int(processed_answer[1])}")
+                print(f"Shot coordinate is {processed_answer[0]}, {processed_answer[1]}")
                 flag = False
